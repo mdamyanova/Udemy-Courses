75b5544078d3b7c292e60ba68957826fde0567c8
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-03 15:54:45 +0300

    Refactor

--- React Native - The Practical Guide/rn-meals/screens/FavoritesScreen.js
+++ React Native - The Practical Guide/rn-meals/screens/FavoritesScreen.js
@@ -4,7 +4,6 @@ import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 import MealList from '../components/MealList';
 import { MEALS } from '../data/dummy-data';
-import { Icon } from 'react-native-paper/typings/components/List';
 const FavoritesScreen = props => {
     const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
 
@@ -18,7 +17,7 @@ FavoritesScreen.navigationOptions = (navData) => {
         headerTitle: 'My Favorites',
         headerLeft: (
             <HeaderButtons HeaderButtonComponents={HeaderButton}>
-                <Icon title='Menu' iconName='ios-menu' onPress={() => {
+                <Item title='Menu' iconName='ios-menu' onPress={() => {
                     navData.navigation.toggleDrawer();
                 }} />
             </HeaderButtons>
