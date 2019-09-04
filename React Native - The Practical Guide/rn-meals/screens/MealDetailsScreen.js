52757c4a8d5f8d4a5573aa3d1071312d1a95ac6d
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-04 15:59:05 +0300

    Work on Meal Details Screen

--- React Native - The Practical Guide/rn-meals/screens/MealDetailsScreen.js
+++ React Native - The Practical Guide/rn-meals/screens/MealDetailsScreen.js
@@ -1,21 +1,44 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { ScrollView, Image, View, Text, Button, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
 import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
+
+const ListItem = props => {
+    return (
+        <View style={styles.listItem}>
+            <DefaultText>{props.children}</DefaultText>
+        </View>
+    );
+}
 
 const MealDetailsScreen = props => {
     const mealId = props.navigation.getParam('mealId');
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
     return (
-        <View style={styles.screen}>
-            <Text>{selectedMeal.title}</Text>
-            <Button title='GO BACK TO CATEGOTIES' onPress={() => {
-                props.navigation.popToTop();
-            }} />
-        </View>
+        <ScrollView>
+            <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+            <View style={styles.details}>
+                <DefaultText>{selectedMeal.duration}m</DefaultText>
+                <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+                <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+            </View>
+            <Text style={styles.title}>Ingredients</Text>
+            {selectedMeal.ingredients.map(ingredient => {
+                return (
+                    <ListItem key={ingredient}>{ingredient}</ListItem>
+                );
+            })}
+            <Text style={styles.title}>Steps</Text>
+            {selectedMeal.steps.map(step => {
+                return (
+                    <ListItem key={step}>{step}</ListItem>
+                )
+            })}
+        </ScrollView>
     );
 };
 
@@ -35,10 +58,26 @@ MealDetailsScreen.navigationOptions = (navigationData) => {
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+        width: '100%',
+        height: 200
+    },
+    details: {
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'space-around'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    listItem: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: '#CCC',
+        borderWidth: 1,
+        padding: 10
     }
 });
 
