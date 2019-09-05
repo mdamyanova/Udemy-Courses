a3a0d738ff017b38adbc9500c5cdd5d9268a5e89
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-05 11:32:45 +0300

    Configuring Redux

--- React Native - The Practical Guide/rn-meals/App.js
+++ React Native - The Practical Guide/rn-meals/App.js
@@ -3,10 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 import { useScreens } from 'react-native-screens';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import MealsNavigator from './navigation/MealsNavigator';
+import mealsReducer from './store/reducers/meals';
 
 useScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer
+});
+
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -24,10 +33,8 @@ export default function App() {
   }
 
   return (
-    <MealsNavigator />
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-
-});
