d4196b35036b7a710ab00a8deeaa1d7ccafe18af
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-05 16:37:09 +0300

    Finish app

--- React Native - The Practical Guide/rn-meals/screens/FiltersScreen.js
+++ React Native - The Practical Guide/rn-meals/screens/FiltersScreen.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useDispatch } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
 import Colors from '../constants/Colors';
+import { setFilters } from '../store/actions/meals';
 
 const FilterSwitch = props => {
   return (
@@ -27,16 +29,18 @@ const FiltersScreen = props => {
   const [isVegan, setIsVegan] = useState(false);
   const [isVegetarian, setIsVegetarian] = useState(false);
 
+  const dispatch = useDispatch();
+
   const saveFilters = useCallback(() => {
     const appliedFilters = {
       glutenFree: isGlutenFree,
       lactoseFree: isLactoseFree,
       vegan: isVegan,
-      isVegetarian: isVegetarian
+      vegetarian: isVegetarian
     };
 
-    console.log(appliedFilters);
-  }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+    dispatch(setFilters(appliedFilters));
+  }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian, dispatch]);
 
   useEffect(() => {
     navigation.setParams({ save: saveFilters });
--- React Native - The Practical Guide/rn-meals/store/reducers/meals.js
+++ React Native - The Practical Guide/rn-meals/store/reducers/meals.js
@@ -1,5 +1,5 @@
 import { MEALS } from '../../data/dummy-data';
-import { TOGGLE_FAVORITE } from '../actions/meals';
+import { TOGGLE_FAVORITE, SET_FILTERS } from '../actions/meals';
 
 const initialState = {
     meals: MEALS,
@@ -20,6 +20,24 @@ const mealsReducer = (state = initialState, action) => {
                 const meal = state.meals.find(meal => meal.id === action.mealId);
                 return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) };
             }
+        case SET_FILTERS:
+            const appliedFilters = action.filters;
+            const updatedFilteredMeals = state.meals.filter(meal => {
+                if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+                    return false;
+                }
+                if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
+                    return false;
+                }
+                if (appliedFilters.vegan && !meal.isVegan) {
+                    return false;
+                }
+                if (appliedFilters.vegetarian && !meal.isVegetarian) {
+                    return false;
+                }
+                return true;
+            });
+            return { ...state, filteredMeals: updatedFilteredMeals };
         default:
             return state;
     }
