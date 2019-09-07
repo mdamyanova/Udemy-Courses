dfc5470416cbd30dc9222bcedc17ed6b390cd786
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-07 18:27:08 +0300

    Work on adding items to the Cart

--- React Native - The Practical Guide/rn-shop/screens/shop/ProductsOverviewScreen.js
+++ React Native - The Practical Guide/rn-shop/screens/shop/ProductsOverviewScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { FlatList } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import ProductItem from '../../components/shop/ProductItem';
+import * as cartActions from '../../store/actions/cart';
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
+  const dispatch = useDispatch();
+
   return (
     <FlatList
       data={products}
@@ -21,7 +24,9 @@ const ProductsOverviewScreen = props => {
               productTitle: itemData.item.title
             });
           }}
-          onAddToCart={() => {}}
+          onAddToCart={() => {
+            dispatch(cartActions.addToCart(itemData.item));
+          }}
         />
       )}
     />
