fba6dc5d99e9baf83faae192697642a8c89975d5
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-07 18:05:39 +0300

    Add fonts

--- React Native - The Practical Guide/rn-shop/screens/shop/ProductDetailsScreen.js
+++ React Native - The Practical Guide/rn-shop/screens/shop/ProductDetailsScreen.js
@@ -40,9 +40,11 @@ const styles = StyleSheet.create({
         fontSize: 20,
         color: '#888',
         textAlign: 'center',
-        marginVertical: 20
+        marginVertical: 20,
+        fontFamily: 'open-sans-bold'
     },
     description: {
+        fontFamily: 'open-sans',
         fontSize: 14,
         textAlign: 'center',
         marginHorizontal: 20
