27664fbf9f84919867709c6bdcbab48e855df029
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-10 18:47:15 +0300

    Fetch Products from the Server

--- React Native - The Practical Guide/rn-shop/store/actions/products.js
+++ React Native - The Practical Guide/rn-shop/store/actions/products.js
@@ -1,6 +1,30 @@
 export const DELETE_PRODUCT = 'DELETE_PRODUCT';
 export const CREATE_PRODUCT = 'CREATE_PRODUCT';
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+
+export const fetchProducts = () => {
+  return async dispatch => {
+    const response = await fetch(
+      'https://rn-shop-12606.firebaseio.com/products.json'
+    );
+
+    const responseData = await response.json();
+    const loadedProducts = [];
+
+    for (const key in responseData) {
+      loadedProducts.push(new Product(
+        key,
+        'u1',
+        responseData[key].title,
+        responseData[key].imageUrl,
+        responseData[key].description,
+        responseData[key].price));
+    }
+
+    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+  }
+};
 
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId };
@@ -36,14 +60,14 @@ export const createProduct = (title, description, imageUrl, price) => {
   }
 };
 
-  export const updateProduct = (id, title, description, imageUrl) => {
-    return {
-      type: UPDATE_PRODUCT,
-      pid: id,
-      productData: {
-        title,
-        description,
-        imageUrl,
-      }
-    };
+export const updateProduct = (id, title, description, imageUrl) => {
+  return {
+    type: UPDATE_PRODUCT,
+    pid: id,
+    productData: {
+      title,
+      description,
+      imageUrl,
+    }
   };
+};
