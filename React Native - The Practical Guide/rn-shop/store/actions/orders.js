7085a1e7ed69d3d6aeaa2fb5b63c6cf0a6de134e
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-11 15:19:15 +0300

    Fetch Stored Orders

--- React Native - The Practical Guide/rn-shop/store/actions/orders.js
+++ React Native - The Practical Guide/rn-shop/store/actions/orders.js
@@ -1,4 +1,38 @@
+import Order from '../../models/order';
+
 export const ADD_ORDER = 'ADD_ORDER';
+export const SET_ORDERS = 'SET_ORDERS';
+
+export const fetchOrders = () => {
+  return async dispatch => {
+    try {
+      const response = await fetch(
+        'https://rn-shop-12606.firebaseio.com/orders/u1.json',
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const resData = await response.json();
+      const loadedOrders = [];
+
+      for (const key in resData) {
+        loadedOrders.push(
+          new Order(
+            key,
+            resData[key].cartItems,
+            resData[key].totalAmount,
+            new Date(resData[key].date)
+          )
+        );
+      }
+      dispatch({ type: SET_ORDERS, orders: loadedOrders });
+    } catch (err) {
+      throw err;
+    }
+  };
+};
 
 export const addOrder = (cartItems, totalAmount) => {
   return async dispatch => {
