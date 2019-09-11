2c2c9ceda16d312659d29af85674c51f3f64236d
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-11 11:48:55 +0300

    Store Orders

--- React Native - The Practical Guide/rn-shop/store/actions/orders.js
+++ React Native - The Practical Guide/rn-shop/store/actions/orders.js
@@ -1,8 +1,37 @@
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount }
+  return async dispatch => {
+    const date = new Date();
+    const response = await fetch(
+      'https://rn-shop-12606.firebaseio.com/orders/u1.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date.toISOString()
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date: date
+      }
+    });
   };
 };
