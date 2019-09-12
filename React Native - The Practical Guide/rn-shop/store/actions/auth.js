030aa2e506d997e20fd46943c24469656a17f4e7
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-12 11:04:50 +0300

    Add User Login

--- React Native - The Practical Guide/rn-shop/store/actions/auth.js
+++ React Native - The Practical Guide/rn-shop/store/actions/auth.js
@@ -1,4 +1,5 @@
 export const SIGNUP = "SIGNUP";
+export const LOGIN = "LOGIN";
 
 export const signup = (email, password) => {
   return async dispatch => {
@@ -24,5 +25,32 @@ export const signup = (email, password) => {
     const resData = await response.json();
     console.log(resData);
     dispatch({ type: SIGNUP });
+  };
+};
+
+export const login = (email, password) => {
+  return async dispatch => {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAL5VGmZZCK5E7Igt56rrHrp3g3YRVTBeQ",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong.");
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+    dispatch({ type: LOGIN });
   };
 };
