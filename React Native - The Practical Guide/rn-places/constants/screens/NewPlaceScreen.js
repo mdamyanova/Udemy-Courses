bed30fcb06d032c74ba89e11c113e7a43a743a9a
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-13 15:08:43 +0300

    Work on New Place Screen

--- React Native - The Practical Guide/rn-places/constants/screens/NewPlaceScreen.js
+++ React Native - The Practical Guide/rn-places/constants/screens/NewPlaceScreen.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  ScrollView,
+  View,
+  Button,
+  Text,
+  TextInput,
+  StyleSheet
+} from 'react-native';
+
+import Colors from '../constants/Colors';
 
 const NewPlaceScreen = props => {
-    return <View>
-        <Text></Text>
-    </View>
+  const [titleValue, setTitleValue] = useState('');
+
+  const titleChangeHandler = text => {
+    setTitleValue(text);
+  };
+
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleChangeHandler}
+          value={titleValue}
+        />
+        <Button title="Save Place" color={Colors.primary} onPress={() => {}} />
+      </View>
+    </ScrollView>
+  );
+};
+
+NewPlaceScreen.navigationOptions = {
+  headerTitle: 'Add Place'
 };
 
 const styles = StyleSheet.create({
-
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
+  }
 });
 
 export default NewPlaceScreen;
