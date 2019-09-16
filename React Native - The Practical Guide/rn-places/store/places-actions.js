14db8c53cebd5343f281378aabd10bee7c274447
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-16 11:19:54 +0300

    Store the Image on the File System

--- React Native - The Practical Guide/rn-places/store/places-actions.js
+++ React Native - The Practical Guide/rn-places/store/places-actions.js
@@ -1,5 +1,22 @@
+import * as FileSystem from "expo-file-system";
+
 export const ADD_PLACE = "ADD_PLACE";
 
 export const addPlace = (title, image) => {
-  return { type: ADD_PLACE, placeData: { title: title, image: image } };
+  return async dispatch => {
+    const fileName = image.split("/").pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: image,
+        to: newPath
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+
+    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
+  };
 };
