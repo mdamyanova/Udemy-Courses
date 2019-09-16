fedc7f82af2146524bab5cf001de2b4d4e481fa4
Mirela Damyanova <mdamyanova181@gmail.com>
2019-09-16 11:47:20 +0300

    Store Data in the Local Database

--- React Native - The Practical Guide/rn-places/helpers/db.js
+++ React Native - The Practical Guide/rn-places/helpers/db.js
@@ -19,3 +19,21 @@ export const init = () => {
   });
   return promise;
 };
+
+export const insertPlace = (title, imageUri, address, lat, lng) => {
+  const promise = new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        "INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?);",
+        [title, imageUri, address, lat, lng],
+        (_, result) => {
+          resolve(result);
+        },
+        (_, err) => {
+          reject(err);
+        }
+      );
+    });
+  });
+  return promise;
+};
--- React Native - The Practical Guide/rn-places/store/places-actions.js
+++ React Native - The Practical Guide/rn-places/store/places-actions.js
@@ -1,6 +1,7 @@
 import * as FileSystem from "expo-file-system";
 
 export const ADD_PLACE = "ADD_PLACE";
+import { insertPlace } from "../helpers/db";
 
 export const addPlace = (title, image) => {
   return async dispatch => {
@@ -12,11 +13,21 @@ export const addPlace = (title, image) => {
         from: image,
         to: newPath
       });
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        "Dummy address",
+        15.6,
+        12.3
+      );
+      console.log(dbResult);
+      dispatch({
+        type: ADD_PLACE,
+        placeData: { id: dbResult.insertId, title: title, image: newPath }
+      });
     } catch (err) {
       console.log(err);
       throw err;
     }
-
-    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
   };
 };
